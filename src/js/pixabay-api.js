ae509bb383fcb786f92606be1b8d76c07327de1e
LaResistancePL <158176161+LaResistancePL@users.noreply.github.com>
2024-12-27 21:01:05 +0100

    bbb

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,23 +1,78 @@
-const API_KEY = '47705929-ee1ce2532f2b45b66a6f43010';
+// Described in the documentation
+import iziToast from 'izitoast';
+// Optional import of styles
+import 'izitoast/dist/css/iziToast.min.css';
 
-function getImg(searchValue) {
-  const searchParams = new URLSearchParams({
-    key: API_KEY,
-    q: searchValue,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
+import { renderPhotos } from './render-functions.js';
 
-  return fetch(`https://pixabay.com/api/?${searchParams.toString()}`).then(
-    res => {
-      if (!res.ok) {
-        throw new Error(res.status);
+var API_KEY = '47376754-ed05d64ddcb60a2a475f4c9e7';
+const searchButton = document.querySelector('button');
+const loader = document.querySelector('span');
+
+export function fetchFunction(
+  q,
+  image_type = 'photo',
+  orientation = 'horizontal',
+  safesearch = true
+) {
+  loader.style.display = 'inline-block';
+  fetch(
+    'https://pixabay.com/api/?key=' +
+      API_KEY +
+      '&q=' +
+      encodeURIComponent(q) +
+      '&image_type=' +
+      encodeURIComponent(image_type) +
+      '&orientation=' +
+      encodeURIComponent(orientation) +
+      '&safesearch=' +
+      encodeURIComponent(safesearch)
+  )
+    .then(response => {
+      // Response handling
+      response => response.json();
+      //   console.log(response);
+      return response.json();
+    })
+    .then(data => {
+      // Data handling
+      data => data.json();
+      //   console.log(data);
+
+      if (data.totalHits > 0) {
+        // console.log(data.hits);
+        data.hits.forEach(element => {
+          console.log(
+            element.largeImageURL,
+            element.previewURL,
+            element.tags,
+            element.likes,
+            element.views,
+            element.comments,
+            element.downloads
+          );
+          renderPhotos(
+            element.largeImageURL,
+            element.previewURL,
+            element.tags,
+            element.likes,
+            element.views,
+            element.comments,
+            element.downloads
+          );
+        });
+      } else {
+        iziToast.error({
+          title: 'Error',
+          message: 'No results for this search',
+          position: 'topRight',
+        });
       }
-
-      return res.json();
-    }
-  );
+      console.log('xd');
+      searchButton.disabled = false;
+      loader.style.display = 'none';
+    })
+    .catch(error => {
+      // Error handling
+    });
 }
-
-export { getImg };
--- src/main.js
+++ src/main.js
@@ -1,73 +1,22 @@
-import { getImg } from './js/pixabay-api';
-import { createCardsMarkup } from './js/render-functions';
+// Described in the documentation
 import iziToast from 'izitoast';
+// Optional import of styles
 import 'izitoast/dist/css/iziToast.min.css';
 
-const fetchUsersBtn = document.querySelector('button[type=submit]');
-const imgs = document.querySelector('.images-div');
-const loaderClass = document.querySelector('.loaderClass');
+import { fetchFunction } from './js/pixabay-api.js';
 
-fetchUsersBtn.addEventListener('click', handleSearch);
+const searchButton = document.querySelector('button');
+const inputBar = document.querySelector('input');
 
-function handleSearch(evt) {
-  evt.preventDefault();
-  let searchInput = document.querySelector('input[name="search"]');
-  let notFoundTextEl = document.querySelector('.not-found-img');
-  let searchValue = searchInput.value.trim();
-
-  if (searchValue <= 0) {
-    iziToast.show({
-      title: '❌',
-      message:
-        'Sorry, there are no images matching your search query. Please try again!',
-      color: 'ef4040',
+searchButton.addEventListener('click', event => {
+  searchButton.disabled = true;
+  if (!inputBar.value) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter search value',
+      position: 'topRight',
     });
-    return;
+  } else {
+    fetchFunction(inputBar.value);
   }
-
-  loaderClass.style.display = 'flex';
-
-  getImg(searchValue)
-    .then(data => {
-      if (data.total === 0) {
-        imgs.innerHTML = '';
-        notFoundTextEl.innerHTML = `Results for query <span>${searchValue}</span> not found!`;
-        iziToast.show({
-          title: '❌',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          backgroundColor: '#ef4040',
-          messageColor: 'white',
-        });
-        return;
-      }
-
-      // Очищення тексту "not found" перед відображенням нових результатів
-      notFoundTextEl.innerHTML = '';
-
-      createCardsMarkup(data.hits);
-
-      const imageElements = document.querySelectorAll('.gallery-img');
-      const loadPromises = Array.from(imageElements).map(img => {
-        return new Promise(resolve => {
-          img.onload = resolve;
-          img.onerror = resolve;
-        });
-      });
-
-      Promise.all(loadPromises)
-        .then(() => {
-          console.log(loadPromises);
-        })
-        .catch(err => {
-          loaderClass.innerHTML = '';
-          console.error(err);
-        });
-    })
-    .catch(console.error)
-    .finally(() => {
-      console.log('completed');
-      loaderClass.style.display = 'none';
-    });
-  searchInput.value = '';
-}
+});
